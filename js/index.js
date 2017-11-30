c752e637c3eb4a7b75a59d1a49dc29ca21d5e4f3
Ali <aliegseem@gmail.com>
2017-11-30 02:27:55 -0500

    Fix uploading on mobile

--- js/index.js
+++ js/index.js
@@ -10,20 +10,56 @@ angularFire.controller("base64Ctrl", function($scope, $firebaseArray) {
   var _validFileExtensions = [".jpg", ".jpeg", ".bmp", ".gif", ".png"];
 
   $scope.uploadPost = function() {
-    var description = $("#nameDescription").val();
-    $scope.posts.$add({
-      date: Firebase.ServerValue.TIMESTAMP,
-      base64: window.localStorage.getItem("uploadedPic"),
-      description: description,
-      location: JSON.parse(window.localStorage.getItem("userLoc")),
-      locName: window.localStorage.getItem("locTag"),
-      role: window.localStorage.getItem('userRole'),
-      username: window.localStorage.getItem('userName'),
-      profilePic: 'https://68.media.tumblr.com/avatar_d5db9f21cb9c_128.png'
-    }).then(result =>{
-      window.location.href = './post_detail.html?postId=' + result.key();            
-    });
-  }
+    
+        var sFileName = $("#nameImg").val();
+        var description = $("#nameDescription").val();
+        if (sFileName.length > 0) {
+    
+          var blnValid = false;
+    
+          for (var j = 0; j < _validFileExtensions.length; j++) {
+    
+            var sCurExtension = _validFileExtensions[j];
+    
+            if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) {
+    
+              blnValid = true;
+              var filesSelected = document.getElementById("nameImg").files;
+              
+              if (filesSelected.length > 0) {
+                var fileToLoad = filesSelected[0];
+                var fileReader = new FileReader();
+    
+                fileReader.onload = function(fileLoadedEvent) {
+
+                  $scope.posts.$add({
+                    date: Firebase.ServerValue.TIMESTAMP,
+                    base64: fileLoadedEvent.target.result,
+                    description: description,
+                    location: JSON.parse(window.localStorage.getItem("userLoc")),
+                    locName: window.localStorage.getItem("locTag"),
+                    role: window.localStorage.getItem('userRole'),
+                    username: window.localStorage.getItem('userName'),
+                    profilePic: 'https://68.media.tumblr.com/avatar_d5db9f21cb9c_128.png'
+                  }).then(result =>{
+                    window.location.href = './post_detail.html?postId=' + result.key();            
+                  });
+                };
+    
+                fileReader.readAsDataURL(fileToLoad);
+              }
+              break;
+            }
+          }
+    
+          if (!blnValid) {
+            alert('File is not valid');
+            return false;
+          }
+        }
+    
+        return true;
+    }
 
   $scope.deletepost = function(postid) {
     var r = confirm("Do you want to remove this image ?");
