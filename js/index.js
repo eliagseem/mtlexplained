59fe7a470a68a69ad31250d876cb71c1c53b7c55
Ali <aliegseem@gmail.com>
2017-11-29 23:25:31 -0500

    uuuugh it's done?

--- js/index.js
+++ js/index.js
@@ -10,58 +10,19 @@ angularFire.controller("base64Ctrl", function($scope, $firebaseArray) {
   var _validFileExtensions = [".jpg", ".jpeg", ".bmp", ".gif", ".png"];
 
   $scope.uploadPost = function() {
-
-    var sFileName = $("#nameImg").val();
     var description = $("#nameDescription").val();
-    if (sFileName.length > 0) {
-
-      var blnValid = false;
-
-      for (var j = 0; j < _validFileExtensions.length; j++) {
-
-        var sCurExtension = _validFileExtensions[j];
-
-        if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) {
-
-          blnValid = true;
-          var filesSelected = document.getElementById("nameImg").files;
-          
-          if (filesSelected.length > 0) {
-            var fileToLoad = filesSelected[0];
-            var fileReader = new FileReader();
-
-            fileReader.onload = function(fileLoadedEvent) {
-              var textAreaFileContents = document.getElementById(
-                "textAreaFileContents"
-              );
-
-              $scope.posts.$add({
-                date: Firebase.ServerValue.TIMESTAMP,
-                base64: fileLoadedEvent.target.result,
-                description: description,
-                location: userLoc,
-                locName: locTag,
-                role: 'Student',
-                username: 'presentation4u',
-                profilePic: 'https://68.media.tumblr.com/avatar_d5db9f21cb9c_128.png'
-              }).then(result =>{
-                window.location.href = './post_detail.html?postId=' + result.key();            
-              });
-            };
-
-            fileReader.readAsDataURL(fileToLoad);
-          }
-          break;
-        }
-      }
-
-      if (!blnValid) {
-        alert('File is not valid');
-        return false;
-      }
-    }
-
-    return true;
+    $scope.posts.$add({
+      date: Firebase.ServerValue.TIMESTAMP,
+      base64: window.localStorage.getItem("uploadedPic"),
+      description: description,
+      location: JSON.parse(window.localStorage.getItem("userLoc")),
+      locName: window.localStorage.getItem("locTag"),
+      role: window.localStorage.getItem('userRole'),
+      username: window.localStorage.getItem('userName'),
+      profilePic: 'https://68.media.tumblr.com/avatar_d5db9f21cb9c_128.png'
+    }).then(result =>{
+      window.location.href = './post_detail.html?postId=' + result.key();            
+    });
   }
 
   $scope.deletepost = function(postid) {
@@ -76,5 +37,4 @@ angularFire.controller("base64Ctrl", function($scope, $firebaseArray) {
       });
     }
   }
-
 });
